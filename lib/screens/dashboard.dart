16be8fa13048b28752a1cacd881e4e2c02567291
Shahed <shahed_hridoy@outlook.com>
2023-01-26 15:28:29 +0600

    network image

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -13,7 +13,7 @@ class Dashboard extends StatelessWidget {
         title: Text('Dashboard'.toUpperCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Image.asset("images/sample_image.jpg")
+      body: Image.network("https://avatars.githubusercontent.com/u/35490924?s=400&u=ef029c933960d1a1dac66af28f7344585b7b93c8&v=4")
     );
   }
 }
