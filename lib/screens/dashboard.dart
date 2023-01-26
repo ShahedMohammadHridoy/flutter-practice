55f894da9d04cf91e9831a1f9427542c5e3837ec
Shahed <shahed_hridoy@outlook.com>
2023-01-26 15:44:18 +0600

    material icon

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -13,10 +12,18 @@ class Dashboard extends StatelessWidget {
         title: Text('Dashboard'.toUpperCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Image.network("https://avatars.githubusercontent.com/u/35490924?s=400&u=ef029c933960d1a1dac66af28f7344585b7b93c8&v=4")
+      backgroundColor: Colors.lightGreen,
+      body: const Center(
+        child: Icon(
+          Icons.alt_route_rounded,
+          size: 100,
+          color: Colors.lightGreenAccent,
+        ),
+      ),
     );
   }
 }
 
-
-int getNumber(){ return Random().nextInt(10); }
+int getNumber() {
+  return Random().nextInt(10);
+}
