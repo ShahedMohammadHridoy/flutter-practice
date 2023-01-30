50eac12320703d5dbd92b3a3c7fcf4cb335f1b1b
Shahed <shahed_hridoy@outlook.com>
2023-01-30 15:19:36 +0600

    Text Direction

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,22 +7,25 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton.icon(
-          icon: const Icon(Icons.add),
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red,
-            padding: const EdgeInsets.all(20.0),
-            elevation: 15,
-            shape: const StadiumBorder(),
-            fixedSize: const Size(300, 80),
-            textStyle: const TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+        child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.add),
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                padding: const EdgeInsets.all(20.0),
+                elevation: 15,
+                shape: const StadiumBorder(),
+                fixedSize: const Size(300, 80),
+                textStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              label: const Text("Add New"),
             ),
-          ),
-          label: const Text("Add New"),
-        ),
+        )
       ),
     );
   }
